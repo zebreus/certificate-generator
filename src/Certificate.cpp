16a82d73fcd58013d1a083b9b241abe113652efe
Lennart Eichhorn <lennart@madmanfred.com>
2019-11-22 16:10:43 +0100

    Changed latex container in Certificate.

--- src/Certificate.cpp
+++ src/Certificate.cpp
@@ -101,7 +101,7 @@ vector<string> Certificate::generateLatexArguments(const filesystem::path& worki
 		user.append(to_string(getuid()));
 		arguments.push_back(user);
 		arguments.push_back("--cap-drop=ALL");
-		arguments.push_back("alpinexetex");
+		arguments.push_back("madmanfred/alpine-xetex");
 	}
 	arguments.push_back("xelatex");
 	arguments.push_back("-halt-on-error");
